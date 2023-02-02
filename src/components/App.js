017c6f18135742c1691f7550958fe7fbc4642e9a
Yash Kaskar <yash.kaskar99@gmail.com>
2023-02-02 13:56:32 +0530

    exact path /

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,28 @@
 import React from 'react'
 import { Container } from 'react-bootstrap';
 import { AuthProvider } from '../context/Authcontext';
-
 import Signup from './Singup';
+import Dashboard from './Dashboard';
+import { BrowserRouter as Router, Routes , Route } from "react-router-dom"
 
 export default function App() {
   return (
     <div> 
       <AuthProvider>  
       < Container className='d-flex align-items-center justify-content-center' style={{minHeight: "100vh"}}>
-        <div className="w-100" style={{maxWidth: "400px"}}>
-        < Signup />
+          <div className="w-100" style={{ maxWidth: "400px" }}>
+            
+          <Router>
+          <AuthProvider>
+            <Routes>
+            <Route exact path="/" element={ <Dashboard />} />
+              <Route path="/signup" element={ <Signup />} />
+              
+            </Routes>
+          </AuthProvider>
+        </Router>
+       
+        
         </div>
       </Container>
       </AuthProvider>
